786dbf1fb9870c4d479518bd2fb8a3b7452fc5e0
marcosdiego1904 <76596163+marcosdiego1904@users.noreply.github.com>
2025-02-27 11:27:35 -0500

    viteconfig update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'lamp-to-my-feet-4.onrender.com',
+      // any other hosts you want to allow
+    ]
+  }
 })
